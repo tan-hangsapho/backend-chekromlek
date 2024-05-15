f05ebf87c25f0c355561fb7ec3dca858bed139b9
Chunming <chunming200@gmail.com>
2024-05-15 08:50:07 +0700

    Fetch

--- notification/src/utils/server.ts
+++ notification/src/utils/server.ts
@@ -1,10 +1,10 @@
 
-import { startQueue } from '@notifications/queues/connection';
 import getConfig from './config';
 import EmailSender from './email-sender';
 import { logInit, logger } from './logger';
 import NodemailerEmailApi from './nodemailer-email-api';
 import { app } from '../app';
+import { startQueue } from '@notifications/queues/connection';
 
 export async function run() {
   try {
---
+++ post/src/database/model/post.model.ts
@@ -0,0 +1,8 @@
+import mongoose from 'mongoose';
+
+export interface IPost {
+ _id?: string;
+ title: string;
+ content?: string;
+  
+}
